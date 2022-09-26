602421b495ce6b490cfe3dc05db9d22f56d83c42
Memo <luisg1287@gmail.com>
2022-09-25 20:45:19 -0500

    Trminamos el frontend

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -1,13 +1,25 @@
 import './App.scss';
 import { DatePicker} from 'antd';
+import { Card } from 'antd';
 
 function App() {
+  const test = (date, dateString) =>{
+    console.log(date, dateString);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
       
        <h1>Se inicio el servidor a la perfección</h1>
-       <DatePicker/>
+       <DatePicker onChange={test}/>
+
+       
+       <Card title="Default size card" extra={<a href="#">More</a>} style={{ width: 300 }}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
       </header>
     </div>
   );
